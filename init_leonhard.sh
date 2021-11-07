1a967501ab445f03ae0c7795890def8669434164
Clement Guerner <cguerner@student-net-vpn-1795.intern.ethz.ch>
2021-11-07 16:55:00 +0100

    added init_leonhard.sh

---
+++ init_leonhard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+module load eth_proxy gcc/8.2.0 python_gpu cudnn/8.1.0.77 bedtools2
+
+#if [ ! -d "venv/" ]; then
+#    python3 -m venv venv
+#    echo "Created virtual environment."
+#fi
+
+#export TA_CACHE_DIR="/scratch/$USER/.cache"
+
+#source venv/bin/activate
+#pip3 install --default-timeout=100 -r requirements.txt
