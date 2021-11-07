2cea3480ca1a768fbc1cb7bd7993de2b9ee70fb8
Clement Guerner <cguerner@student-net-vpn-1795.intern.ethz.ch>
2021-11-07 16:58:30 +0100

    updated paths in constants.py

--- src/splicing/splicing/utils/constants.py
+++ src/splicing/splicing/utils/constants.py
@@ -1,3 +1,5 @@
+import os
+
 CL_max=400  # original 10000
 # Maximum nucleotide context length (CL_max/2 on either side of the 
 # position of interest)
@@ -8,12 +10,12 @@ SL=5000  # original 5000
 # SL will be the output length)
 
 # Input details
-data_dir='/home/anej/repos/studies/deep-learning-project/data/'
+data_dir=os.path.join(os.environ['SOURCE'], "dl_data/spliceai")
 
-splice_table='/home/anej/repos/studies/deep-learning-project/data/gtex_dataset.txt'
-ref_genome='/home/anej/repos/studies/deep-learning-project/data/genome.fa'
+splice_table=os.path.join(data_dir, 'gtex_dataset.txt')
+ref_genome=os.path.join(data_dir, 'genome.fa')
 
 # Output details
-sequence='/home/anej/repos/studies/deep-learning-project/data/gtex_sequence.txt'
+sequence=os.path.join(data_dir, 'gtex_sequence.txt')
 
 # module load eth_proxy gcc/8.2.0 python_gpu cudnn/8.1.0.77 bedtools2
